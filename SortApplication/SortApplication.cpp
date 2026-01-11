50f4aabdc9e31cdf67d1004fb14e28853789cd88
GlassDream <copy54993@outlook.com>
2026-01-12 00:24:48 +0800

    第一次测试

--- SortApplication/SortApplication.cpp
+++ SortApplication/SortApplication.cpp
@@ -1,9 +1,17 @@
 #include "SortApplication.h"
+#include "qpushbutton.h"
+#include "qmessagebox.h"
 
 SortApplication::SortApplication(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    this->resize(350, 350);
+    QPushButton* button = new QPushButton(this);
+    button->setGeometry(50, 50, 200, 200);
+    button->setText("专用按钮");
+    connect(button, &QPushButton::clicked, this, [=]() { QMessageBox::information(this, "test", "测试用例"); });
 }
 
 SortApplication::~SortApplication()
