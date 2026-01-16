a79ac01b696b873b57389c1959a3f4bf32629c8a
GlassDream <copy54993@outlook.com>
2026-01-16 17:57:16 +0800

    完善文件管理对象

--- SortApplication/Form/ManagerMent.cpp
+++ SortApplication/Form/ManagerMent.cpp
@@ -1,14 +1,25 @@
 #include "ManagerMent.h"
-/*bool ManagerMent::SaveTheFilePath(string url)
-{
-	/*filePath = url;
-	if (!(filePath.empty()))
-		return true;
-	else*/
-	//	return false;
-//}
 
-/*string ManagerMent::GetFilePath()
+ManagerMent::ManagerMent(QObject* parent = nullptr) : QObject(parent), _filePath("")
 {
-	return filePath;
-}*/
+	//私有化构造函数用于处理对象
+}
+
+ManagerMent* ManagerMent::GetInstance()
+{
+	//获取返回的单例对象指针
+	static ManagerMent instance;
+	return &instance;
+}
+
+void ManagerMent::SaveFilesPath(string url)
+{
+	//存入文件路径
+	this->_filePath = url;
+}
+
+string ManagerMent::GetFilesPath()
+{
+	//获取文件路径
+	return this->_filePath;
+}
