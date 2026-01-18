852fbebaa9708eb2101c5ac394a11ed2a4c3c4ba
dmwywy <25011944@edu.muc.cn>
2026-01-18 19:53:15 +0800

    UI03:制作了部分Function功能的界面

--- SortApplication/UI/SortApplication.cpp
+++ SortApplication/UI/SortApplication.cpp
@@ -1,15 +1,30 @@
 #include "SortApplication.h"
-#include "qpushbutton.h"
-#include "qmessagebox.h"
+#include <QTimer>
 
-SortApplication::SortApplication(QWidget *parent)
+SortApplication::SortApplication(QWidget* parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
 
-    
+
+    //分别连接两个QRadioButton和QStackedWidget的两页（lambda）
+    connect(ui.sortRadioButton, &QRadioButton::toggled, this, [=](bool checked) {
+        if (checked) {
+            ui.stackedWidget->setCurrentIndex(0);
+        }
+    });
+    connect(ui.renameRadioButton, &QRadioButton::toggled, this, [=](bool checked) {
+        if (checked) {
+            ui.stackedWidget->setCurrentIndex(1);
+        }
+    });
+
+    //设置默认状态
+    ui.sortRadioButton->setChecked(true);
+
+
 }
 
 SortApplication::~SortApplication()
-{}
-
+{
+}
