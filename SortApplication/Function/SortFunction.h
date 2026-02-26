0e70d958c047e276399b6360e235e5912cb5fad4
GlassDream <copy54993@outlook.com>
2026-02-26 17:14:27 +0800

    (13)Form:添加了用户输入路径的获取与存入接口 (4)SortFunction:完善了SortFunction可能编写的分类函数

--- SortApplication/Function/SortFunction.h
+++ SortApplication/Function/SortFunction.h
@@ -19,13 +19,22 @@ public:
 	//文件名筛选分类函数
 	bool SortFileByFileName();
 
+	//重命名函数
+	
+	//添加前缀命名
+	bool RenameFileByPrefix();
+	//统一后缀命名
+	bool RenameFileBySuffix();
+	//统一名称命名
+	bool RenameFileByKeyWord();
+
 public slots:
 	//分类槽函数 - 监听ManagerMent - StartOperator()信号
 	//判断使用的分类方法
 	bool SureSortOperator();
 	//启动撤回函数
 	bool WithDrawOperator();
-
+	
 private:
 	ManagerMent* manager;
 };
